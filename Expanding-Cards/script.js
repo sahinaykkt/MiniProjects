622684aefcc0d8aabbbf89f0a674bff70fd9d77a
Aykut Şahin <59021102+sahinaykkt@users.noreply.github.com>
2020-12-25 01:02:58 +0300

    Update script.js

--- Expanding-Cards/script.js
+++ Expanding-Cards/script.js
@@ -1,1 +1,18 @@
+const panels = document.querySelectorAll(".panel");
 
+for (let i = 0; i < panels.length; i++) {
+    panels[i].addEventListener("click", function () {
+        if (panels[i].classList.contains("active")) {
+            removeActive();
+        } else {
+            removeActive();
+            panels[i].classList.add("active");
+        }
+    })
+};
+
+function removeActive() {
+    for (let k = 0; k < panels.length; k++) {
+        panels[k].classList.remove("active");
+    }
+}
